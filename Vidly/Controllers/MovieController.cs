4cd3c047f6bbf5b85b34ada373245ae4d6f9a149
Kishore kumar <kishore3886@gmail.com>
2020-12-02 18:33:12 -0800

    day 1 commit

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name="Bahubali" };
+
+            var customers = new List<Customer> 
+            { 
+                   new Customer{Name="Kishore"}
+                  // new Customer{Name="Naren"}
+
+            };
+
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+
+            };
+            //return Content("hello world");
+            //return HttpNotFound();
+
+
+            //return new EmptyResult();
+
+            //name of the action, controller, parameters (anonymous object)
+            //return RedirectToAction("Index","Home", new { page=1, sortBy= "Name" } );
+            return View(viewModel);
+        }
+    }
+}
