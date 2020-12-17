5ad6b1f8ee01bf188444b23a05f00272ed04996a
Kishore kumar <kishore3886@gmail.com>
2020-12-17 07:24:50 -0800

    customer forms added with validation, antiforgerytoken

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,8 +10,12 @@ namespace Vidly.Models
     {
 
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
+        //[Required]
         public bool IsSubscribedToNewsLetter { get; set; }
 
         public MembershipType membershipType { get; set; }
@@ -19,7 +23,9 @@ namespace Vidly.Models
         [Display(Name = "Membership")]
         public byte MembershipTypeId { get; set; }
 
+        
         [Display(Name="Date Of Birth")]
+        [Min18YearsIfMember]
         public DateTime? Dob { get; set; }
     }
 }
